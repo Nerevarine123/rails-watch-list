3321a26331c353064826b137b157d8a1ad2c7e88
Usman Ali <usmanv5@hotmail.com>
2023-11-23 13:55:31 +0000

    list create/new finished show placeholder done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,28 @@
 class ListsController < ApplicationController
-
   def index
     @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save == true
+      redirect_to lists_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = @list.bookmarks
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
